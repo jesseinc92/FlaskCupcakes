dc8b652f6c2d670420234da072c56ada16a85006
Jesse Hill <jesse.o.hill@gmail.com>
2021-10-03 17:44:57 -0400

    implemented through part one

---
+++ app.py
@@ -0,0 +1,15 @@
+"""Flask app for Cupcakes"""
+
+from flask import Flask
+from models import db, connect_db, Cupcake
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'yessiree'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+connect_db(app)
+db.create_all()
+
+
